ec682a819f98f7617fa9dea0376ec8bc3a236b8a
Denis Tatar <denis.tatar8@gmail.com>
2022-01-26 23:55:33 -0500

    more practice

--- practice.cc
+++ practice.cc
@@ -36,6 +36,64 @@ public:
             }
         }
     }
+
+    int strStr(string haystack, string needle) {
+        int result;
+        
+        if (haystack == needle) {
+            result = 0;
+        } else if (needle.size() == 0 || haystack.size() == 0){
+            result = -1;
+        } else {
+            result = haystack.find(needle);
+        }
+        
+        return result;
+        // Side not: hey, it's ok. Keep your head up.
+    }
+
+    // Not finished yet
+    string reverseWords(string s) {
+        string result;        
+        string word;
+        string reverseWord;
+
+
+        // Traverse through the list and find each word 
+        for (int i = 0; i < s.length(); i++){
+            if (s[i] == ' ' || i == s.length() - 1){
+                // Word is now complete, we need to reßverse it.
+                cout << word << endl;
+                for (int j = word.size() + 1; j > 0; j--){
+                    reverseWord = reverseWord + word[j];
+                    cout << word[j] << endl; 
+                }
+                cout << reverseWord << endl;
+
+                // Word is successfully reversed, check if last word and add to result string
+                if (i == s.length() - 1){
+                    result = result + reverseWord;
+                    break;
+                } else {
+                    reverseWord = reverseWord + ' ';
+                    result = result + reverseWord;
+                    reverseWord = "";
+                }
+
+                
+                
+                // Replace what is in word with an empty string and repeat the process
+                word = "";
+            } else {
+                // Keeping adding to the word vector so it can be reversed
+                word += s[i];
+            }
+        }
+
+        // Return our final answer
+        cout << "Result is: " << result << endl;
+        return result;
+    }
 };
 
 
@@ -50,5 +108,8 @@ int main(){
     array.push_back(12);
     sols.twoSum(array, 9);
 
-    sols.prettyPrint(array); 
+    // sols.prettyPrint(array); 
+    
+    sols.reverseWords("Let's take LeetCode contest");
+    // "s'teL ekat edoCteeL tsetnoc"
 }
